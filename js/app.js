9e85c84fc9cfb6d7f6ffd6d08f6fb59568f7b01a
Dioniscio <dionisciosepulveda@gmail.com>
2019-09-25 20:42:56 -0600

    Mi Segundo Commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
+var url = window.location.href;
+
+var SWlocation = '/twittor/sw.js';
 
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+    if(url.includes('localhost')){
+        navigator.serviceWorker.register('/sw.js');
+    }
+    
+    navigator.serviceWorker.register(SWlocation);
 }
 
 
